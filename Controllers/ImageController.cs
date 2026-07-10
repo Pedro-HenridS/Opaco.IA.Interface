fe1dc53522848deb383c6c532cdf2e72652a980d
Pedro-HenridS <pedrosouza9479@gmail.com>
2026-07-10 00:02:33 -0300

    feat: creating endpoint for image generation from prompts

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Opaco.IA.Interface.Controllers
+{
+    public class ImageController : Controller
+    {
+        // Inicialmente, proponho um enpoint enxuto, apenas para requisição de prompt
+        [HttpPost("/generate-image")]
+        public IActionResult GenerateImage([FromBody] string prompt)
+        {
+            return Ok();
+        }
+    }
+}
